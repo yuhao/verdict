cc6962a9b20b99ae057a212aa47fc632ce0c5dce
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-07-14 18:12:20 +0000

    Add robust (in the argmax sense) checking.

--- mnist.py
+++ mnist.py
@@ -4,11 +4,14 @@
 
 from z3 import *
 from numpy import genfromtxt
+import time
 
 set_option(rational_to_decimal=True)
+set_option("verbose", 10)
 
 reluC = RealVal(0.01)
 
+print "*****Creating Weights*****"
 weights1 = genfromtxt('mnist/para/weights1.csv', delimiter=',')
 weights2 = genfromtxt('mnist/para/weights2.csv', delimiter=',')
 weights3 = genfromtxt('mnist/para/weights3.csv', delimiter=',')
@@ -20,11 +23,11 @@ W1 = [ [ RealVal(weights1[i][j]) for j in range(l1_n) ] for i in range(l0_n) ]
 W2 = [ [ RealVal(weights2[i][j]) for j in range(l2_n) ] for i in range(l1_n) ]
 W3 = [ [ RealVal(weights3[i][j]) for j in range(l3_n) ] for i in range(l2_n) ]
 
-print "*****Weights Created*****"
 print float(W1[l0_n - 1][l1_n - 1].as_decimal(20)), weights1[l0_n - 1][l1_n - 1]
 print float(W2[l1_n - 1][l2_n - 1].as_decimal(20)), weights2[l1_n - 1][l2_n - 1]
 print float(W3[l2_n - 1][l3_n - 1].as_decimal(20)), weights3[l2_n - 1][l3_n - 1]
 
+print "*****Creating Biases*****"
 biases1 = genfromtxt('mnist/para/biases1.csv', delimiter=',')
 biases2 = genfromtxt('mnist/para/biases2.csv', delimiter=',')
 biases3 = genfromtxt('mnist/para/biases3.csv', delimiter=',')
@@ -33,12 +36,11 @@ B1 = [ RealVal(biases1[i]) for i in range(l1_n) ]
 B2 = [ RealVal(biases2[i]) for i in range(l2_n) ]
 B3 = [ RealVal(biases3[i]) for i in range(l3_n) ]
 
-print "*****Biases Created*****"
 print float(B1[l1_n - 1].as_decimal(20)), biases1[l1_n - 1]
 print float(B2[l2_n - 1].as_decimal(20)), biases2[l2_n - 1]
 print float(B3[l3_n - 1].as_decimal(20)), biases3[l3_n - 1]
 
-print "*****Start Solving*****"
+print "*****Creating Assertions*****"
 InX = [ Real('inX-%s' % i) for i in range(l0_n) ]
 InY= [ Real('inY-%s' % i) for i in range(l0_n) ]
 L1X = [ Real('l1X-%s' % i) for i in range(l1_n) ]
@@ -47,6 +49,13 @@ L2X = [ Real('l2X-%s' % i) for i in range(l2_n) ]
 L2Y = [ Real('l2Y-%s' % i) for i in range(l2_n) ]
 OutX = [ Real('outX-%s' % i) for i in range(l3_n) ]
 OutY = [ Real('outY-%s' % i) for i in range(l3_n) ]
+
+def robust(X, Y, n):
+  # robust(X, Y) is equivalent to assert argmax(X) == argmax(Y)
+  return Or( [ And( [ And( X[j] >= X[i], Y[j] >= Y[i] ) for i in range(n) if i != j ] ) for j in range(n) ] )
+
+def unique(X, n):
+  return And([X[i] != X[j] for i in range(n) for j in range(i) if j != i])
 
 def vvmul(V1, V2, n):
   res = 0
@@ -57,9 +66,10 @@ def vvmul(V1, V2, n):
 def vmmul(V, M, O, n):
   res = [None] * n
   for i in range(0, n):
-    ### Use ReLU for activatation
     tmp = vvmul(M[i], V, n)
-    res[i] = If(tmp > 0, O[i] == tmp, O[i] == 0)
+    ### Use ReLU for activatation
+    res[i] = (O[i] == tmp)
+    #res[i] = If(tmp > 0, O[i] == tmp, O[i] == 0)
     #res[i] = If(tmp >= 0, O[i] == tmp, O[i] == tmp * reluC)
   return res
 
@@ -71,9 +81,12 @@ l1_y_cond = vmmul(InY, W1, L1Y, l1_n)
 l2_y_cond = vmmul(L1Y, W2, L2Y, l2_n)
 out_y_cond = vmmul(L2Y, W3, OutY, l3_n)
 
-input_cond = [ And(0 <= InY[i] - InX[i], InY[i] - InX[i] < 0.0001, 0 <= InY[i], InY[i] <= 1, 0 <= InX[i], InX[i] <= 1) for i in range(l0_n) ]
+input_cond = [ And(0 < InY[i] - InX[i], InY[i] - InX[i] < 0.0001, 0 <= InY[i], InY[i] <= 1, 0 <= InX[i], InX[i] <= 1) for i in range(l0_n) ]
 
-output_cond = [ (OutY[i] - OutX[i] > 2) for i in range(l3_n) ]
+# This is a necessary but not sufficient constraint for negating robustness (for classification)
+output_cond = [ Or( [ OutY[i] - OutX[i] > 1 for i in range(l3_n) ] ) ]
+# This is a precise constraint for negating robustness, but more complex to solve
+#output_cond = [ Not(robust(OutX, OutY, l3_n)) ]
 
 s = Solver()
 s.add(l1_x_cond +
@@ -84,9 +97,17 @@ s.add(l1_x_cond +
       out_y_cond +
       input_cond +
       output_cond)
+#asserts = s.assertions()
+#print len(asserts), "constraints"
 
-if (s.check() == sat):
+print "*****Start Solving*****"
+startTime = time.time()
+result = s.check()
+duration = time.time() - startTime
+
+if (result == sat):
   m = s.model()
   print m
 else:
   print s.check()
+print duration
