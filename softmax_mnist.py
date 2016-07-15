4de43f9311b5de64d10b62ef9f984a737cc830a8
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-07-15 21:44:24 +0000

    Add support for verification mode (general/specific).

--- softmax_mnist.py
+++ softmax_mnist.py
@@ -1,5 +1,3 @@
-# Copyright (c) Microsoft Corporation 2015
-
 #inputL(l0) --> outputL(l1)
 
 from z3 import *
@@ -9,7 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Verdict harness.")
 parser.add_argument("-i", "--input-perturbation",
-                    dest="input_pert",
+                    dest="input_bound",
                     type=float,
                     help="input perturbation. choose between (0, 1)",
                     default=0.0001)
@@ -22,42 +20,23 @@ parser.add_argument("-o", "--output-bound",
                     type=int,
                     help="output bound. invalid when robustness is strong",
                     default=1)
+parser.add_argument("-m", "--verify-mode",
+                    dest="verify_mode",
+                    help="verification mode. choose between <general, specific>",
+                    default="general")
 
 args = parser.parse_args()
-input_pert = args.input_pert
+input_bound = args.input_bound
 robust_cons = args.robust_cons
 output_bound = args.output_bound
+verify_mode = args.verify_mode
 
-print "*****Using Parameters*****"
+print "\n=====Parameter List====="
 for arg in vars(args):
   print '[' + arg + ']:', getattr(args, arg)
 
 set_option(rational_to_decimal=True)
 set_option("verbose", 10)
-
-print "\n*****Creating Weights*****"
-weights = np.genfromtxt('mnist/para/softmax_weights.csv', delimiter=',')
-
-l0_n, l1_n = weights.shape #748, 10
-weights = np.transpose(weights)
-
-# W = 10_row * 784_col
-W = [ [ RealVal(weights[i][j]) for j in range(l0_n) ] for i in range(l1_n) ]
-
-print float(W[l1_n - 1][l0_n - 1].as_decimal(20)), weights[l1_n - 1][l0_n - 1]
-
-print "\n*****Creating Biases*****"
-biases = np.genfromtxt('mnist/para/softmax_biases.csv', delimiter=',')
-
-B = [ RealVal(biases[i]) for i in range(l1_n) ]
-
-print float(B[l1_n - 1].as_decimal(20)), biases[l1_n - 1]
-
-print "\n*****Creating Assertions*****"
-InX = [ Real('inX-%s' % i) for i in range(l0_n) ]
-InY= [ Real('inY-%s' % i) for i in range(l0_n) ]
-OutX = [ Real('outX-%s' % i) for i in range(l1_n) ]
-OutY = [ Real('outY-%s' % i) for i in range(l1_n) ]
 
 def convertToPythonNum(num):
   if is_real(num) == True:
@@ -112,33 +91,49 @@ def vmmul(V, M, B, O, m, n):
     res[i] = (O[i] == tmp)
   return res
 
+def solveIt(s):
+  #print "=====Start Solving====="
+  startTime = time.time()
+  result = s.check()
+  duration = time.time() - startTime
+  print "[Solver Runtime] %.2f %s" % (duration, result)
+
+  if (result == sat):
+    m = s.model()
+    #print m
+    print "argmax(OutX)", np.argmax([convertToPythonNum(m.evaluate(OutX[i])) for i in range(l1_n)])
+    print "argmax(OutY)", np.argmax([convertToPythonNum(m.evaluate(OutY[i])) for i in range(l1_n)])
+
+print "\nCreating Weights"
+weights = np.genfromtxt('mnist/para/softmax_weights.csv', delimiter=',')
+l0_n, l1_n = weights.shape #748, 10
+weights = np.transpose(weights)
+# W = 10_row * 784_col
+W = [ [ RealVal(weights[i][j]) for j in range(l0_n) ] for i in range(l1_n) ]
+
+print "Creating Biases"
+biases = np.genfromtxt('mnist/para/softmax_biases.csv', delimiter=',')
+B = [ RealVal(biases[i]) for i in range(l1_n) ]
+
+print "Creating Assertions"
+if verify_mode == "specific":
+  # The MNIST data set has 10,000 images for testing
+  inputs = np.genfromtxt('mnist/para/mnist_test_images_100.csv', delimiter=',')
+  InX = [ RealVal(inputs[0][i]) for i in range(l0_n) ]
+else:
+  InX= [ Real('inX-%s' % i) for i in range(l0_n) ]
+InY= [ Real('inY-%s' % i) for i in range(l0_n) ]
+OutX = [ Real('outX-%s' % i) for i in range(l1_n) ]
+OutY = [ Real('outY-%s' % i) for i in range(l1_n) ]
+
 out_x_cond = vmmul(InX, W, B, OutX, l0_n, l1_n)
-
 out_y_cond = vmmul(InY, W, B, OutY, l0_n, l1_n)
-
-#TODO: The input pertubation constriants have to be more general
-input_cond = [ And(0 < InY[i] - InX[i], InY[i] - InX[i] < input_pert, 0 < InY[i], InY[i] < 1, 0 < InX[i], InX[i] < 1) for i in range(l0_n) ]
-
+input_cond = [ And(InX[i] - InY[i] < input_bound, InY[i] - InX[i] < input_bound, 0 <= InY[i], InY[i] <= 1, 0 <= InX[i], InX[i] <= 1) for i in range(l0_n) ]
 output_cond = [ Not( robust(OutX, OutY, l1_n) ) ]
 
 s = Solver()
 s.add(out_x_cond +
-      out_y_cond +
+      out_y_cond+
       input_cond +
       output_cond)
-asserts = s.assertions()
-print len(asserts), "constraints"
-
-print "\n*****Start Solving*****"
-startTime = time.time()
-result = s.check()
-duration = time.time() - startTime
-print "[Runtime]", duration
-
-if (result == sat):
-  m = s.model()
-  print m
-  print "argmax(OutX)", np.argmax([convertToPythonNum(m.evaluate(OutX[i])) for i in range(l1_n)])
-  print "argmax(OutY)", np.argmax([convertToPythonNum(m.evaluate(OutY[i])) for i in range(l1_n)])
-else:
-  print result
+solveIt(s)
