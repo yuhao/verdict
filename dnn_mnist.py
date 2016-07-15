f6827a5410e02cdad854fe351e49136811f35c59
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-07-15 17:04:06 +0000

    Update convertToPythonNum, robust, and argparse of dnn_mnist.

--- dnn_mnist.py
+++ dnn_mnist.py
@@ -15,12 +15,12 @@ parser.add_argument("-i", "--input-perturbation",
                     default=0.0001)
 parser.add_argument("-r", "--robustness",
                     dest="robust_cons",
-                    help="robustness constraint. choose between <weak, medium, strong>",
-                    default="strong")
-parser.add_argument("-o", "--output-perturbation",
-                    dest="output_pert",
+                    help="robustness constraint. choose between <imprecise, precise>",
+                    default="precise")
+parser.add_argument("-o", "--output-bound",
+                    dest="output_bound",
                     type=int,
-                    help="output tolerance. invalid when robustness is strong",
+                    help="output bound. invalid when robustness is strong",
                     default=1)
 parser.add_argument("-a", "--activation-function",
                     dest="act_func",
@@ -30,10 +30,10 @@ parser.add_argument("-a", "--activation-function",
 args = parser.parse_args()
 input_pert = args.input_pert
 robust_cons = args.robust_cons
-output_pert = args.output_pert
+output_bound = args.output_bound
 act_func = args.act_func
 
-print "*****Using Parameters*****"
+print "\n*****Using Parameters*****"
 for arg in vars(args):
   print '[' + arg + ']:', getattr(args, arg)
 
@@ -43,7 +43,7 @@ set_option("verbose", 10)
 natureExp = RealVal(math.e)
 reluC = RealVal(0.01)
 
-print "*****Creating Weights*****"
+print "\n*****Creating Weights*****"
 weights1 = np.genfromtxt('mnist/para/weights1.csv', delimiter=',')
 weights2 = np.genfromtxt('mnist/para/weights2.csv', delimiter=',')
 weights3 = np.genfromtxt('mnist/para/weights3.csv', delimiter=',')
@@ -63,7 +63,7 @@ print float(W1[l1_n - 1][l0_n - 1].as_decimal(20)), weights1[l1_n - 1][l0_n - 1]
 print float(W2[l2_n - 1][l1_n - 1].as_decimal(20)), weights2[l2_n - 1][l1_n - 1]
 print float(W3[l3_n - 1][l2_n - 1].as_decimal(20)), weights3[l3_n - 1][l2_n - 1]
 
-print "*****Creating Biases*****"
+print "\n*****Creating Biases*****"
 biases1 = np.genfromtxt('mnist/para/biases1.csv', delimiter=',')
 biases2 = np.genfromtxt('mnist/para/biases2.csv', delimiter=',')
 biases3 = np.genfromtxt('mnist/para/biases3.csv', delimiter=',')
@@ -76,7 +76,7 @@ print float(B1[l1_n - 1].as_decimal(20)), biases1[l1_n - 1]
 print float(B2[l2_n - 1].as_decimal(20)), biases2[l2_n - 1]
 print float(B3[l3_n - 1].as_decimal(20)), biases3[l3_n - 1]
 
-print "*****Creating Assertions*****"
+print "\n*****Creating Assertions*****"
 InX = [ Real('inX-%s' % i) for i in range(l0_n) ]
 InY= [ Real('inY-%s' % i) for i in range(l0_n) ]
 L1X = [ Real('l1X-%s' % i) for i in range(l1_n) ]
@@ -89,16 +89,40 @@ OutY = [ Real('outY-%s' % i) for i in range(l3_n) ]
 def convertToPythonNum(num):
   if is_real(num) == True:
     if is_rational_value(num) == True:
-      denom = num.denominator()
-      numerator = num.numerator()
-      return float(numerator.as_string()) / float(denom.as_string())
+      # 1 doesn't end with '?'
+      if num.as_decimal(10).endswith('?'):
+        return float(num.as_decimal(10)[:-1])
+      else:
+        return float(num.as_decimal(10))
     else:
+      # |approx_num| is the approx rational of the irrational |num|
+      # |approx_num| is guaranteed to end with a '?'
       approx_num = num.approx(5)
-      denom = approx_num.denominator()
-      numerator = approx_num.numerator()
-      return float(numerator.as_string()) / float(denom.as_string())
+      return float(approx_num.as_decimal(10)[:-1])
   else:
     return float(num.as_string())
+
+def robust(X, Y, n):
+  if robust_cons == "imprecise":
+    # This is an imprecise constraint. It asserts that a model is robust if
+    # *all* labels' errors are bound by |output_bound|. Depending on the value
+    # of |output_bound|, this constraint could be more relaxed or stricter than
+    # the precise constraint. Relaxed or strict, it is easier to solve.
+    return And( [ And(Y[i] - X[i] < output_bound, X[i] - Y[i] < output_bound) for i in range(n) ] )
+  else:
+    # This is the precise constraint for robustness, but more complex to solve.
+    # It is equivalent to assert argmax(X) == argmax(Y). Note that if there are
+    # multiple occurrences of the max value, the standard argmax will return
+    # only the first occurrence.  In that case, this |robust| implementation
+    # for checking robustness is incorrect. For example, if X = [4, 4, 4] and Y
+    # = [3, 5, 3], then argmax(X) is 0 and argmax(Y) is 1.  In this case,
+    # argmax(X) != argmax(Y) while robust(X, Y, 3) is still true because
+    # And(X[1] >= X[0], X[1] >= X[2], Y[1] >= Y[0], Y[1] >= Y[2]) == True.
+    return Or( [ And( [ And( X[j] >= X[i], Y[j] >= Y[i] ) for i in range(n) if i != j ] ) for j in range(n) ] )
+
+# To Make sure all elements in X are different
+def unique(X, n):
+  return And([X[i] != X[j] for i in range(n) for j in range(i) if j != i])
 
 def sigmoid(x):
   res = 1 / (1 + natureExp**(-x))
@@ -109,20 +133,6 @@ def approx_sigmoid(x):
   approxExp = 1 - x + x**2 / 2 - x**3/6
   res = 1 / (1 + approxExp)
   return res
-
-def robust(X, Y, n):
-  # robust(X, Y) is equivalent to assert argmax(X) == argmax(Y). Note that if
-  # there are multiple occurrences of the max value, the standard argmax will
-  # return only the first occurrence.  In that case, this |robust|
-  # implementation for checking robustness is incorrect. For example, if
-  # X = [4, 4, 4] and Y = [3, 5, 3], then argmax(X) is 0 and argmax(Y) is 1.
-  # In this case, argmax(X) != argmax(Y) while robust(X, Y, 3) is still true
-  # because And(X[1] >= X[0], X[1] >= X[2], Y[1] >= Y[0], Y[1] >= Y[2]) == True.
-  return Or( [ And( [ And( X[j] >= X[i], Y[j] >= Y[i] ) for i in range(n) if i != j ] ) for j in range(n) ] )
-
-# To Make sure all elements in X are different
-def unique(X, n):
-  return And([X[i] != X[j] for i in range(n) for j in range(i) if j != i])
 
 def vvmul(V1, V2, bias, n):
   res = 0
@@ -159,19 +169,7 @@ out_y_cond = vmmul(L2Y, W3, B3, OutY, l2_n, l3_n)
 #TODO: The input pertubation constriants have to be more general
 input_cond = [ And(0 < InY[i] - InX[i], InY[i] - InX[i] < input_pert, 0 < InY[i], InY[i] < 1, 0 < InX[i], InX[i] < 1) for i in range(l0_n) ]
 
-if robust_cons == "weak":
-  # This is the weakest constraint. It asserts that the new output is wrong
-  # only when all labels are off by |output_pert|.
-  output_cond = [ OutY[i] - OutX[i] > output_pert for i in range(l3_n) ]
-elif robust_cons == "medium":
-  # This is stronger than the weak constraint, but is a necessary but
-  # insufficient constraint for negating robustness. It asserts an output as
-  # wrong if any of the label is off by |output_pert|.
-  output_cond = [ Or( [ OutY[i] - OutX[i] > output_pert for i in range(l3_n) ] ) ]
-else:
-  # This is the precise constraint for negating robustness (see comments for
-  # |robust| implementation for details), but more complex to solve.
-  output_cond = [ Not( robust(OutX, OutY, l3_n) ) ]
+output_cond = [ Not( robust(OutX, OutY, l3_n) ) ]
 
 s = Solver()
 s.add(l1_x_cond +
@@ -185,7 +183,7 @@ s.add(l1_x_cond +
 asserts = s.assertions()
 print len(asserts), "constraints"
 
-print "*****Start Solving*****"
+print "\n*****Start Solving*****"
 startTime = time.time()
 result = s.check()
 duration = time.time() - startTime
@@ -197,4 +195,4 @@ if (result == sat):
   print "argmax(OutX)", np.argmax([convertToPythonNum(m.evaluate(OutX[i])) for i in range(l3_n)])
   print "argmax(OutY)", np.argmax([convertToPythonNum(m.evaluate(OutY[i])) for i in range(l3_n)])
 else:
-  print s.check()
+  print result
