d9a99f3c335f9f564ef6cc85477cd5e8ea59bf5e
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-07-20 00:49:23 -0500

    Import verdict_core to softmax_dnn. Redesign its argparse. Add option to choose between scaled and unscaled inputs.

--- softmax_mnist.py
+++ softmax_mnist.py
@@ -2,15 +2,15 @@
 
 from z3 import *
 import numpy as np
-import time
 import argparse
+import verdict_core as core
 
 parser = argparse.ArgumentParser(description="Verdict harness.")
-parser.add_argument("-i", "--input-perturbation",
-                    dest="input_bound",
-                    type=int,
-                    help="input perturbation. choose between (0, 255)",
-                    default=4)
+parser.add_argument("-i", "--input-info",
+                    dest="input_info",
+                    nargs=2,
+                    help="input info. a tuple of [scaled/unscaled, perturbation]. choose perturbation between [0, 255] if unscaled or [0, 1] if scaled)",
+                    default=['scaled', '0.004'])
 parser.add_argument("-r", "--robustness",
                     dest="robust_cons",
                     help="robustness constraint. choose between <imprecise, precise>",
@@ -31,7 +31,9 @@ parser.add_argument("-c", "--input-id",
                     default=0)
 
 args = parser.parse_args()
-input_bound = args.input_bound
+input_info = args.input_info
+input_scaled = input_info[0]
+input_var = float(input_info[1]) if input_scaled == "scaled" else int(input_info[1])
 robust_cons = args.robust_cons
 output_bound = args.output_bound
 verify_mode = args.verify_mode
@@ -44,87 +46,12 @@ for arg in vars(args):
 set_option(rational_to_decimal=True)
 set_option("verbose", 10)
 
-def scaleInput(X, scale, n):
-  # x is an Int array, scale is Real, res will be a Real array
-  res = [None] * n
-  for i in range(n):
-    res[i] = X[i] / scale
-  return res
-
-def convertToPythonNum(num):
-  if is_real(num) == True:
-    if is_rational_value(num) == True:
-      # 1 doesn't end with '?'
-      if num.as_decimal(10).endswith('?'):
-        return float(num.as_decimal(10)[:-1])
-      else:
-        return float(num.as_decimal(10))
-    else:
-      # |approx_num| is the approx rational of the irrational |num|
-      # |approx_num| is guaranteed to end with a '?'
-      approx_num = num.approx(5)
-      return float(approx_num.as_decimal(10)[:-1])
-  else:
-    return float(num.as_string())
-
-def robust(X, Y, n):
-  if robust_cons == "imprecise":
-    # This is an imprecise constraint. It asserts that a model is robust if
-    # *all* labels' errors are bound by |output_bound|. Depending on the value
-    # of |output_bound|, this constraint could be more relaxed or stricter than
-    # the precise constraint. Relaxed or strict, it is easier to solve.
-    return And( [ And(Y[i] - X[i] < output_bound, X[i] - Y[i] < output_bound) for i in range(n) ] )
-  else:
-    # This is the precise constraint for robustness, but more complex to solve.
-    # It is equivalent to assert argmax(X) == argmax(Y). Note that if there are
-    # multiple occurrences of the max value, the standard argmax will return
-    # only the first occurrence. In that case, this |robust| implementation
-    # for checking robustness is incorrect. For example, if X = [4, 4, 4] and Y
-    # = [3, 5, 3], then argmax(X) is 0 and argmax(Y) is 1.  In this case,
-    # argmax(X) != argmax(Y) while robust(X, Y, 3) is still true because
-    # And(X[1] >= X[0], X[1] >= X[2], Y[1] >= Y[0], Y[1] >= Y[2]) == True.
-    # See argmax.py for more details.
-    return Or( [ And( [ And( X[j] >= X[i], Y[j] >= Y[i] ) for i in range(n) if i != j ] ) for j in range(n) ] )
-
-# To Make sure all elements in X are different
-def unique(X, n):
-  return And([X[i] != X[j] for i in range(n) for j in range(i) if j != i])
-
-def vvmul(V1, V2, bias, n):
-  res = 0
-  for i in range(0, n):
-    res += V1[i] * V2[i]
-  res += bias
-  return res
-
-# V=[1_row * m_col], M=[n_row * m_col]
-def vmmul(V, M, B, m, n):
-  res = [None] * n
-  for i in range(0, n):
-    res[i] = vvmul(V, M[i], B[i], m)
-  return res
-
 def genCExp(m):
   with file('cexp.csv', 'w') as outfile:
     inx = [convertToPythonNum(m.evaluate(X[i])) for i in range(l0_n)]
     np.savetxt(outfile, np.atleast_2d(inx), delimiter=",")
     iny = [convertToPythonNum(m.evaluate(Y[i])) for i in range(l0_n)]
     np.savetxt(outfile, np.atleast_2d(iny), delimiter=",")
-
-def solveIt():
-  startTime = time.time()
-  result = s.check()
-  duration = time.time() - startTime
-  print "[Solver Runtime] %.2f %s" % (duration, result)
-
-  if (result == sat):
-    m = s.model()
-    #print m
-    print "argmax(OutX)", np.argmax([convertToPythonNum(m.evaluate(OutX[i])) for i in range(l1_n)])
-    print "argmax(OutY)", np.argmax([convertToPythonNum(m.evaluate(OutY[i])) for i in range(l1_n)])
-    print "(OutX)", [convertToPythonNum(m.evaluate(OutX[i])) for i in range(l1_n)]
-    print "(OutY)", [convertToPythonNum(m.evaluate(OutY[i])) for i in range(l1_n)]
-    genCExp(m)
 
 print "\nCreating Weights"
 weights = np.genfromtxt('mnist/para/softmax_weights.csv', delimiter=',')
@@ -138,24 +65,44 @@ biases = np.genfromtxt('mnist/para/softmax_biases.csv', delimiter=',')
 B = [ RealVal(biases[i]) for i in range(l1_n) ]
 
 print "Creating Assertions"
-scale = RealVal(255)
-if verify_mode == "specific":
-  # The MNIST data set has 10,000 images for testing
-  inputs = np.genfromtxt('mnist/para/mnist_test_images_unscaled_100.csv', delimiter=',')
-  X = [ IntVal(inputs[input_id][i]) for i in range(l0_n) ]
-  InX = scaleInput(X, scale, l0_n)
-else:
-  X = [ Int('x%s' % i) for i in range(l0_n) ]
-  InX = scaleInput(X, scale, l0_n)
-Y = [ Int('y%s' % i) for i in range(l0_n) ]
-InY = scaleInput(Y, scale, l0_n)
+if (input_scaled == "scaled"):
+  if verify_mode == "specific":
+    inputs = np.genfromtxt('mnist/para/mnist_test_images_scaled_100.csv', delimiter=',')
+    InX = [ RealVal(inputs[0][i]) for i in range(l0_n) ]
+  else:
+    InX = [ Real('inX-%s' % i) for i in range(l0_n) ]
+  InY= [ Real('inY-%s' % i) for i in range(l0_n) ]
+else: #unscaled
+  scale = RealVal(255)
+  if verify_mode == "specific":
+    inputs = np.genfromtxt('mnist/para/mnist_test_images_unscaled_100.csv', delimiter=',')
+    X = [ IntVal(inputs[input_id][i]) for i in range(l0_n) ]
+    InX = core.scaleInput(X, scale, l0_n)
+  else:
+    X = [ Int('x%s' % i) for i in range(l0_n) ]
+    InX = core.scaleInput(X, scale, l0_n)
+  Y = [ Int('y%s' % i) for i in range(l0_n) ]
+  InY = core.scaleInput(Y, scale, l0_n)
 
-OutX = vmmul(InX, W, B, l0_n, l1_n)
-OutY = vmmul(InY, W, B, l0_n, l1_n)
-input_cond = [ And(X[i] - Y[i] < input_bound, Y[i] - X[i] < input_bound, 0 <= Y[i], Y[i] <= 255, 0 <= X[i], X[i] <= 255) for i in range(l0_n) ]
-output_cond = [ Not( robust(OutX, OutY, l1_n) ) ]
+OutX = core.vmmul(InX, W, B, l0_n, l1_n, "none")
+OutY = core.vmmul(InY, W, B, l0_n, l1_n, "none")
+if input_scaled == "scaled":
+  input_cond = [ And(InX[i] - InY[i] < input_var, InY[i] - InX[i] < input_var, 0 <= InY[i], InY[i] <= 1, 0 <= InX[i], InX[i] <= 1) for i in range(l0_n) ]
+else:
+  input_cond = [ And(X[i] - Y[i] < input_var, Y[i] - X[i] < input_var, 0 <= Y[i], Y[i] <= 255, 0 <= X[i], X[i] <= 255) for i in range(l0_n) ]
+output_cond = [ Not( core.full_robust(OutX, OutY, l1_n, robust_cons) ) ]
 
 s = Solver()
 s.add(input_cond +
       output_cond)
-solveIt()
+result = core.solveIt(s)
+if (result == sat):
+  m = s.model()
+  #print m
+  outx = [simplify(OutX[i]) for i in range(l1_n)]
+  outy = [m.evaluate(OutY[i]) for i in range(l1_n)]
+  print "OutX", outx
+  print "OutY", outy
+  print "argmax(OutX)", np.argmax(outx)
+  print "argmax(OutY)", np.argmax(outy)
+  genCExp(m)
