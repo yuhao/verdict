78456f448c70acb0f9d5b1f8170cd4db8ba3c689
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-07-20 00:48:44 -0500

    Rename dnn_mnist_specific to dnn_mnist_ref. Import verdict_core.

--- dnn_mnist_ref.py
+++ dnn_mnist_ref.py
@@ -4,10 +4,11 @@ from z3 import *
 import numpy as np
 import time
 import argparse
+import verdict_core as core
 
 parser = argparse.ArgumentParser(description="Verdict harness.")
 parser.add_argument("-i", "--input-perturbation",
-                    dest="input_bound",
+                    dest="input_var",
                     type=float,
                     help="input perturbation. choose between (0, 1)",
                     default=0.004)
@@ -18,7 +19,7 @@ parser.add_argument("-c", "--input-id",
                     default=0)
 
 args = parser.parse_args()
-input_bound = args.input_bound
+input_var = args.input_var
 input_id = args.input_id
 
 print "\n=====Parameter List====="
@@ -28,50 +29,14 @@ for arg in vars(args):
 set_option(rational_to_decimal=True)
 set_option("verbose", 10)
 
-def convertToPythonNum(num):
-  if is_real(num) == True:
-    if is_rational_value(num) == True:
-      # 1 doesn't end with '?'
-      if num.as_decimal(10).endswith('?'):
-        return float(num.as_decimal(10)[:-1])
-      else:
-        return float(num.as_decimal(10))
-    else:
-      # |approx_num| is the approx rational of the irrational |num|
-      # |approx_num| is guaranteed to end with a '?'
-      approx_num = num.approx(5)
-      return float(approx_num.as_decimal(10)[:-1])
-  else:
-    return float(num.as_string())
-
-def argmax(X, n):
-  maxId = 0
-  maxVal = X[0]
-  for i in range(1, n):
-    if simplify(X[i] >= maxVal):
-      maxVal = X[i]
-      maxId = i
-  return maxId
-
-def robust(X, Y, n):
-  maxId = argmax(X, n)
-  return [ Y[maxId] >= Y[i] for i in range(n) if i != maxId ]
-
-def vvmul(V1, V2, bias, n):
-  res = 0
-  for i in range(0, n):
-    res += V1[i] * V2[i]
-  res += bias
-  return res
-
 # V=[1_row * m_col], M=[n_row * m_col]
 def vmmul(V1, V2, M, B, m, n, act):
   res1 = [None] * n
   res2 = [None] * n
   cond = [None] * n
   for i in range(0, n):
-    tmp1 = vvmul(V1, M[i], B[i], m)
-    tmp2 = vvmul(V2, M[i], B[i], m)
+    tmp1 = core.vvmul(V1, M[i], B[i], m)
+    tmp2 = core.vvmul(V2, M[i], B[i], m)
 
     if act == True:
       if (simplify(tmp1 > 0)):
@@ -86,14 +51,6 @@ def vmmul(V1, V2, M, B, m, n, act):
       res1[i] = tmp1
       res2[i] = tmp2
   return (res1, res2, cond)
-
-def solveIt():
-  startTime = time.time()
-  result = s.check()
-  duration = time.time() - startTime
-  print "[Solver Runtime] %.2f %s" % (duration, result)
-
-  return result
 
 print "\nCreating Weights"
 weights1 = np.genfromtxt('mnist/para/weights1.csv', delimiter=',')
@@ -121,7 +78,7 @@ B2 = [ RealVal(biases2[i]) for i in range(l2_n) ]
 B3 = [ RealVal(biases3[i]) for i in range(l3_n) ]
 
 print "Creating Assertions"
-inputs = np.genfromtxt('mnist/para/mnist_test_images_2.csv', delimiter=',')
+inputs = np.genfromtxt('mnist/para/mnist_test_images_100.csv', delimiter=',')
 InX = [ RealVal(inputs[input_id][i]) for i in range(l0_n) ]
 InY= [ Real('inY-%s' % i) for i in range(l0_n) ]
 
@@ -130,22 +87,29 @@ L2X, L2Y, cond2 = vmmul(L1X, L1Y, W2, B2, l1_n, l2_n, True)
 OutX, OutY, _ = vmmul(L2X, L2Y, W3, B3, l2_n, l3_n, False)
 
 #TODO: The input pertubation constriants have to be more general
-input_cond = [ And(InX[i] - InY[i] < input_bound, InY[i] - InX[i] < input_bound, 0 <= InY[i], InY[i] <= 1, 0 <= InX[i], InX[i] <= 1) for i in range(l0_n) ]
+input_cond = [ And(InX[i] - InY[i] < input_var, InY[i] - InX[i] < input_var, 0 <= InY[i], InY[i] <= 1, 0 <= InX[i], InX[i] <= 1) for i in range(l0_n) ]
 
-robust_cond = robust(OutX, OutY, l3_n)
+robust_cond = core.ref_robust(OutX, OutY, l3_n)
 output_cond = [ Not(robust_cond[i]) for i in range(l3_n - 1) ]
 
 s = Solver()
 s.add(input_cond)
-result = solveIt()
+result = core.solveIt(s)
 print "[add] %d asts now" % (len(s.assertions()))
 
+# Incrementally adding constraints. The complete constraint takes the
+# following form: IL /\ HL1 /\ HL2 /\ (OL1 \/ OL2 ... \/ OL9), where IL is
+# input_cond, HL1 and HL2 are cond1 and cond2, and OLn is individual formulas in
+# output_cond. The idea is to check IL /\ OL1 first, if unsat, try IL /\ OL2,
+# and so on. Best case we can quickly get unsat and verifies robustness. If
+# IL /\ OL1 is sat, then incrementally add formulas in HL1 and HL2. If unsat,
+# move to next OL, if sat, keep adding them.
 for index, out_cond in enumerate(output_cond):
   print "Adding constraint %d from output layer" % index
   s.push()
   s.add(out_cond)
   print "[push] %d asts now" % (len(s.assertions()))
-  result = solveIt()
+  result = core.solveIt(s)
   if (result == unsat):
     s.pop()
     print "[pop] %d asts now" % (len(s.assertions()))
@@ -159,7 +123,7 @@ for index, out_cond in enumerate(output_cond):
         print "Adding constraint %d from hidden layer 2" % index
       s.add(cond)
       print "[push] %d asts now" % (len(s.assertions()))
-      result = solveIt()
+      result = core.solveIt(s)
       if (result == unsat):
         break
     s.pop()
@@ -173,8 +137,8 @@ if (result == unsat):
   print "Model is robust!"
 else:
   m = s.model()
-  outx = [(m.evaluate(OutX[i])) for i in range(n)]
-  outy = [(m.evaluate(OutY[i])) for i in range(n)]
+  outx = [simplify(OutX[i]) for i in range(l3_n)]
+  outy = [m.evaluate(OutY[i]) for i in range(l3_n)]
   print "OutX", outx
   print "OutY", outy
   print "argmax(OutX)", np.argmax(outx)
