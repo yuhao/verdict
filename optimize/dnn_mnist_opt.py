fe3d3d0f0252f57e8392d27b411661de1d462e53
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-07-22 16:18:34 +0000

    Create the optimize/ directory, which contains dnn_mnist_opt.py that finds the maximal tolerable perturbation.

---
+++ optimize/dnn_mnist_opt.py
@@ -0,0 +1,157 @@
+from __future__ import print_function
+from ortools.linear_solver import linear_solver_pb2
+from ortools.linear_solver import pywraplp
+import numpy as np
+import argparse
+
+def vvmul(V1, V2, bias, n):
+  res = 0.0
+  for i in range(0, n):
+    res += V1[i] * V2[i]
+  res += bias
+  return res
+
+def vmmul_produce_sign(V, M, B, m, n, act):
+  res = [None] * n
+  sign = [None] * n
+  for i in range(0, n):
+    tmp = vvmul(V, M[i], B[i], m)
+    if act == True:
+      res[i] = tmp if(tmp > 0) else 0
+      sign[i] = True if(tmp > 0) else False
+    else:
+      res[i] = tmp
+  return (res, sign)
+
+def vmmul_consume_sign(V, S, M, B, m, n, act):
+  res = [None] * n
+  cond = [None] * n
+  for i in range(0, n):
+    tmp = vvmul(V, M[i], B[i], m)
+
+    if act == True:
+      if (S[i] == True):
+        cond[i] = (-tmp <= 0)
+        res[i] = tmp
+      else:
+        cond[i] = (tmp <= 0) #It's OK to use <=
+        res[i] = 0
+    else:
+      res[i] = tmp
+  return (res, cond)
+
+def FindMaximalRobustness(optimization_problem_type, input_id):
+  """Example of simple linear program with natural language API."""
+  print("\nProcess Weights")
+  weights1 = np.genfromtxt('../mnist/para/weights1.csv', delimiter=',')
+  weights2 = np.genfromtxt('../mnist/para/weights2.csv', delimiter=',')
+  weights3 = np.genfromtxt('../mnist/para/weights3.csv', delimiter=',')
+
+  l0_n, l1_n = weights1.shape #748, 128
+  l2_n, l3_n = weights3.shape #32, 10
+
+  W1 = np.transpose(weights1)
+  W2 = np.transpose(weights2)
+  W3 = np.transpose(weights3)
+
+  print("Process Biases")
+  B1 = np.genfromtxt('../mnist/para/biases1.csv', delimiter=',')
+  B2 = np.genfromtxt('../mnist/para/biases2.csv', delimiter=',')
+  B3 = np.genfromtxt('../mnist/para/biases3.csv', delimiter=',')
+
+  print("Process Reference Input")
+  inputs = np.genfromtxt('../mnist/para/mnist_test_images_100.csv', delimiter=',')
+  InX = inputs[input_id]
+  L1X, hl1_sign = vmmul_produce_sign(InX, W1, B1, l0_n, l1_n, True)
+  L2X, hl2_sign = vmmul_produce_sign(L1X, W2, B2, l1_n, l2_n, True)
+  OutX, _ = vmmul_produce_sign(L2X, W3, B3, l2_n, l3_n, False)
+  maxId = np.argmax(OutX)
+
+  solvers = [ pywraplp.Solver('FindMaximalRobustness%d' % i,
+                  optimization_problem_type) for i in range(l3_n - 1) ]
+  min_delta = 1.0
+  for s in range(l3_n - 1):
+    solver = solvers[s]
+    infinity = solver.infinity()
+
+    print("\nIteration %d: Creating Constraints" % s)
+    InY = [ solver.NumVar(0.0, 1.0, 'y%s' % i) for i in range(l0_n) ]
+    delta = solver.NumVar(0.0, infinity, 'delta')
+
+    solver.Minimize(delta)
+
+    # Create input layer constraints
+    input_cond = [None] * l0_n * 2
+    for i in range (l0_n):
+      input_cond[i] = (InY[i] - InX[i] <= delta)
+      input_cond[i + l0_n] = (InX[i] - InY[i] <= delta)
+
+    # Create hidden layer constraints
+    L1Y, hl1_cond = vmmul_consume_sign(InY, hl1_sign, W1, B1, l0_n, l1_n, True)
+    L2Y, hl2_cond = vmmul_consume_sign(L1Y, hl2_sign, W2, B2, l1_n, l2_n, True)
+    OutY, _ = vmmul_consume_sign(L2Y, None, W3, B3, l2_n, l3_n, False)
+
+    # Create output layer constraints
+    output_cond = [ (OutY[maxId] <= OutY[i] + 0.01) for i in range(l3_n) if i != maxId ]
+
+    # Add all constraints
+    for j in range (l0_n):
+      solver.Add(input_cond[j])
+      solver.Add(input_cond[j + l0_n])
+
+    for j in range(l1_n):
+      solver.Add(hl1_cond[j])
+
+    for j in range(l2_n):
+      solver.Add(hl2_cond[j])
+
+    solver.Add(output_cond[s])
+
+    print("Start solving with %d constraints %d variables" % (solver.NumConstraints(), solver.NumVariables()))
+    result = SolveAndPrint(solver)
+
+    if (result == pywraplp.Solver.OPTIMAL):
+      if (solver.Objective().Value() < min_delta):
+        min_delta = solver.Objective().Value()
+        #OutY_Value = [ OutY[i].solution_value() for i in range(l3_n)]
+      #print("Min Delta:", min_delta)
+
+  #print('OutX =', OutX)
+  #print('OutY =', OutY_Value)
+  #print('argmax(OutX) =', np.argmax(OutX))
+  #print('argmax(OutY) =', np.argmax(OutY_Value))
+
+  return min_delta
+
+def SolveAndPrint(solver):
+  """Solve the problem and print the solution."""
+
+  result_status = solver.Solve()
+
+  if (result_status == pywraplp.Solver.OPTIMAL):
+    # The solution looks legit (when using solvers others than
+    # GLOP_LINEAR_PROGRAMMING, verifying the solution is highly recommended!).
+    if (solver.VerifySolution(1e-7, True)):
+      #print(('Problem solved in %f milliseconds' % solver.wall_time()))
+      # The objective value of the solution.
+      print(('Optimal objective value = %f' % solver.Objective().Value()))
+
+  return result_status
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Find Maximal Perturbation a Model Can Tolerate.")
+  parser.add_argument("-c", "--input-id",
+                      dest="input_id",
+                      type=int,
+                      help="input id",
+                      default=0)
+  args = parser.parse_args()
+  input_id = args.input_id
+
+  max_robustness = FindMaximalRobustness(pywraplp.Solver.GLOP_LINEAR_PROGRAMMING, input_id)
+  print("\nMaximul Tolerable Perturbation is %f (%d)" % (max_robustness, max_robustness * 255))
+
+
+if __name__ == '__main__':
+  main()
